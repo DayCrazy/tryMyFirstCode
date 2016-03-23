36e6b3649642ca5f1e3ed5216852ded5fe6f830e
zhaohongxiang <zhaohongxiang@1660818821.com>
2016-03-23 16:04:06 +0800

    Initial Commit

---
+++ tryGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  tryGit
+//
+//  Created by yinglaijinrong on 16/3/23.
+//  Copyright © 2016年 yinglai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ tryGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tryGit
+//
+//  Created by yinglaijinrong on 16/3/23.
+//  Copyright © 2016年 yinglai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
